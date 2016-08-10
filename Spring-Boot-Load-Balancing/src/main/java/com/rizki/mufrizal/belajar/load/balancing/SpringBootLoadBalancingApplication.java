25df849ac4722efe389ba1f9a5175bc6ef2823e4
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-10 20:05:29 +0700

    initial project spring

---
+++ Spring-Boot-Load-Balancing/src/main/java/com/rizki/mufrizal/belajar/load/balancing/SpringBootLoadBalancingApplication.java
@@ -0,0 +1,12 @@
+package com.rizki.mufrizal.belajar.load.balancing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoadBalancingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoadBalancingApplication.class, args);
+	}
+}
