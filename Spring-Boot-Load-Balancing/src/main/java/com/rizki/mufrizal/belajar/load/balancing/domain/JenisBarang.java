fdb2c34514e94961aa629e82a5cc1ecb90bb6060
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-10 22:32:17 +0700

    tambah enum jenis barang

---
+++ Spring-Boot-Load-Balancing/src/main/java/com/rizki/mufrizal/belajar/load/balancing/domain/JenisBarang.java
@@ -0,0 +1,13 @@
+package com.rizki.mufrizal.belajar.load.balancing.domain;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com> <https://RizkiMufrizal.github.io>
+ * @Since 10 August 2016
+ * @Time 21:21
+ * @Project Spring-Boot-Load-Balancing
+ * @Package com.rizki.mufrizal.belajar.load.balancing.domain
+ * @File JenisBarang
+ */
+public enum JenisBarang {
+    cair, gas, padat
+}
